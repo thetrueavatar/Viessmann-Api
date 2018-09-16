fb672f1b21122f456cbeedd50ed61a4fc34cfca2
thetrueavatar <thetrueavatar@gmail.com>
2018-09-16 18:02:39 +0200

    fixed Main.php created shell script to generate phar update composer.json to contains options of phar-builder tools

--- example/Main.php
+++ example/Main.php
@@ -1,5 +1,6 @@
 <?php
 include __DIR__.'/bootstrap.php';
+use Viessmann\API\ViessmannAPI;
 echo $viessmannApi->getFeatures();
 echo "Température extérieure " . $viessmannApi->getOutsideTemperature() . "\n";
 echo "Température boiler " . $viessmannApi->getBoilerTemperature() . "\n";
