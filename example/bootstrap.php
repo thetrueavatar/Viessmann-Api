12f02453408bf0ed1ec49fbc50c3196ad452b08e
thetrueavatar <thetrueavatar@gmail.com>
2018-10-07 07:00:27 +0200

    edit example bootstrap to show how to override default circuitId and deviceId

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -1,10 +1,11 @@
 <?php
-include 'phar://'.__DIR__.'/Viessmann-Api-0.3-SNAPSHOT.phar/index.php';
+include __DIR__.'/../index.php';
 use Viessmann\API\ViessmannAPI;
 $credentials = file(__DIR__."/credentials.properties");
 $params = [
     "user" => trim("$credentials[0]", "\n"),
     "pwd" => trim("$credentials[1]", "\n"),
-    "uri" => "vicare://oauth-callback/everest"
+    "deviceId" => "0",
+    "circuitId"=>"0"
 ];
 $viessmannApi = new ViessmannAPI($params);
