50f9c8e4fcbb4e70e243d7fe19095ea9ac028794
thetrueavatar <thetrueavatar@gmail.com>
2019-01-06 12:16:25 +0100

    update bootstrap to set custom exception handler to display only interresting message upon exception.

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -6,16 +6,31 @@ use Viessmann\API\ViessmannApiException;
 include 'phar://' . __DIR__ . '/Viessmann-Api-0.4-SNAPSHOT.phar/index.php';
 
 
-$credentials = file(__DIR__."/credentials.properties");
+$credentials = file(__DIR__ . "/credentials.properties");
 $params = [
     "user" => trim("$credentials[0]", "\n"),
     "pwd" => trim("$credentials[1]", "\n"),
     "deviceId" => "0",
-    "circuitId"=>"0"
+    "circuitId" => "0"
 ];
+function print_exception($e){
+    echo "Message: " . $e->getMessage() . "\n";
+    echo "Code: " . $e->getCode() . "\n";
+//    echo "Trace:" . $e->getTraceAsString() . "\n";
+}
+;
+$errorHandler= function($e)
+{
+    $currentException=$e;
+    do {
+        print_exception($currentException);
+    } while ($currentException = $currentException->getPrevious());
+};
+
+set_exception_handler($errorHandler);
 try {
-$viessmannApi = new ViessmannAPI($params);
+    $viessmannApi = new ViessmannAPI($params);
 } catch (ViessmannApiException $e) {
-    echo $e->getMessage();
+    $errorHandler($e);
     exit();
 }
