51c7d0c738c56472b21925130156e91233ceffd7
lbscld <claude.libois@forem.be>
2021-07-19 00:54:12 +0200

    added s to burner

--- src/API/ViessmannFeature.php
+++ src/API/ViessmannFeature.php
@@ -27,11 +27,11 @@ final class ViessmannFeature
     const HEATING_BOILER_SENSORS_TEMPERATURE_MAIN = "heating.boiler.sensors.temperature.main";
     const HEATING_BOILER_SERIAL = "heating.boiler.serial";
     const HEATING_BOILER_TEMPERATURE = "heating.boiler.temperature";
-    const HEATING_BURNER_AUTOMATIC = "heating.burner.automatic";
-    const HEATING_BURNER_CURRENT_POWER = "heating.burner.current.power";
-    const HEATING_BURNER = "heating.burner";
-    const HEATING_BURNER_MODULATION = "heating.burner.0.modulation";
-    const HEATING_BURNER_STATISTICS = "heating.burner.0.statistics";
+    const HEATING_BURNER_AUTOMATIC = "heating.burners.automatic";
+    const HEATING_BURNER_CURRENT_POWER = "heating.burners.current.power";
+    const HEATING_BURNER = "heating.burners";
+    const HEATING_BURNER_MODULATION = "heating.burners.0.modulation";
+    const HEATING_BURNER_STATISTICS = "heating.burners.0.statistics";
     const HEATING_CIRCUITS_0_CIRCULATION = "heating.circuits.0.circulation";
     const HEATING_CIRCUITS_0_CIRCULATION_PUMP = "heating.circuits.0.circulation.pump";
     const HEATING_CIRCUITS_0_CIRCULATION_SCHEDULE = "heating.circuits.0.circulation.schedule";
