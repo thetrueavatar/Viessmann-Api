4d648584ad95b36a918a570eb89f061caf36501c
lbscld <claude.libois@forem.be>
2021-07-26 15:53:09 +0200

    replace php_ini_parser by custom parser to avoid problem with special character.

--- example/bootstrap-dev.php
+++ example/bootstrap-dev.php
@@ -2,7 +2,7 @@
 include __DIR__.'/../index.php';
 use Viessmann\API\ViessmannAPI;
 
-$params = parse_ini_file(__DIR__ . "/credentials.properties");
+$params = parseProperties(file_get_contents(__DIR__ . "/credentials.properties"));
 //$params = [
 //    "user" => trim("$params[0]"),
 //    "pwd" => trim("$params[1]"),
@@ -32,4 +32,31 @@ try {
 } catch (ViessmannApiException $e) {
     $errorHandler($e);
     exit();
+}
+function parseProperties($fileContent) {
+    $result = [];
+    $fileContent = str_replace("\r\n", "\n", $fileContent);
+    $lines = explode("\n", $fileContent);
+    $lastkey = '';
+    $appendNextLine = false;
+    foreach ($lines as $l) {
+        $cleanLine = trim($l);
+        if ($cleanLine === '') continue;
+        if (strpos($cleanLine, '#') === 0) continue; // is comment ... move on
+
+        $endsWithSlash = substr($l, -1) === '\\';
+        if ($appendNextLine) {
+            $result[$lastkey] .= "\n" . substr($l, 0, $endsWithSlash ? -1 : 10000);
+            if (!$endsWithSlash) { // last line of multi-line property does not end with '\' char
+                $appendNextLine = false;
+            }
+        } else {
+            $key = trim(substr($l, 0, strpos($l, '=')));
+            $value = substr($l,strpos($l,'=') + 1, $endsWithSlash ? -1 : 10000);
+            $lastkey = $key;
+            $result[$key] = $value;
+            $appendNextLine = $endsWithSlash;
+        }
+    }
+    return $result;
 }
--- example/bootstrap.php
+++ example/bootstrap.php
@@ -4,9 +4,7 @@ use Viessmann\API\ViessmannAPI;
 use Viessmann\API\ViessmannApiException;
 session_start();
 include 'phar://' . __DIR__ . '/Viessmann-Api-2.1.0-SNAPSHOT.phar/index.php';
-
-
-$params = parse_ini_file(__DIR__ . "/credentials.properties");
+$params = parseProperties(file_get_contents(__DIR__ . "/credentials.properties"));
 function print_exception($e){
     echo "Message: " . $e->getMessage() . "\n";
     echo "Code: " . $e->getCode() . "\n";
@@ -23,8 +21,36 @@ $errorHandler= function($e)
 
 set_exception_handler($errorHandler);
 try {
+    print_r($params);
     $viessmannApi = new ViessmannAPI($params);
 } catch (ViessmannApiException $e) {
     $errorHandler($e);
     exit();
+}
+function parseProperties($fileContent) {
+    $result = [];
+    $fileContent = str_replace("\r\n", "\n", $fileContent);
+    $lines = explode("\n", $fileContent);
+    $lastkey = '';
+    $appendNextLine = false;
+    foreach ($lines as $l) {
+        $cleanLine = trim($l);
+        if ($cleanLine === '') continue;
+        if (strpos($cleanLine, '#') === 0) continue; // is comment ... move on
+
+        $endsWithSlash = substr($l, -1) === '\\';
+        if ($appendNextLine) {
+            $result[$lastkey] .= "\n" . substr($l, 0, $endsWithSlash ? -1 : 10000);
+            if (!$endsWithSlash) { // last line of multi-line property does not end with '\' char
+                $appendNextLine = false;
+            }
+        } else {
+            $key = trim(substr($l, 0, strpos($l, '=')));
+            $value = substr($l,strpos($l,'=') + 1, $endsWithSlash ? -1 : 10000);
+            $lastkey = $key;
+            $result[$key] = $value;
+            $appendNextLine = $endsWithSlash;
+        }
+    }
+    return $result;
 }
